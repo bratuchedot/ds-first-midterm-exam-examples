023506ff38856d2a4c9366aed33cc713826f7ebe
Emilijan Koteski <emilijankoteski@pm.me>
2022-11-19 12:18:09 +0100

    Added chat app example in tcp and udp

---
+++ src/udp/chat/server/UserInfo.java
@@ -0,0 +1,30 @@
+package udp.chat.server;
+
+import java.net.InetAddress;
+
+public class UserInfo {
+    private InetAddress address;
+    private int port;
+
+    public UserInfo(InetAddress address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    public InetAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(InetAddress address) {
+        this.address = address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
+
